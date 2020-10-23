0ab4b9bb4a251aef93364ee16a5362ebb65f3090
Tung Nguyen <tungtn3@gmail.com>
2020-10-23 23:20:55 +1100

    add doc comments to CharGrid

--- src/lib.rs
+++ src/lib.rs
@@ -12,23 +12,43 @@ fn eq_color(a: Color, b: Color) -> bool {
         && (a[3] - b[3]).abs() <= f32::EPSILON
 }
 
+/// A color argument for CharGrid functions that want to set the colors of cells as an option when
+/// writing characters into the grid.
 #[derive(Clone, Copy)]
 pub enum CellColorArg {
+    /// Don't change color.
     Pass,
+    /// Set the cell to use the CharGrid default color.
     Default,
+    /// Set a specific color.
     Color(Color),
 }
 
+/// A CharGrid is a grid of characters that can be drawn onto, and afterwards the whole grid can be
+/// drawn on screen.  The whole CharGrid has default foreground and background colors that cells
+/// can take advantage of, and individual cells can also have custom foreground and background
+/// colors.
+///
+/// To use a CharGrid, create a new one, put and print characters and colors to it, then call draw
+/// to put it all on screen.
 pub struct CharGrid {
+    /// Dimensions of the grid in characters: [width, height].
     size: Size,
+    /// Default foreground color for cells.
     default_fg: Color,
+    /// Default background color for cells.
     default_bg: Color,
+    /// Text character in each cell.
     chars: Vec<char>,
+    /// Foreground in each cell; `None` means use default foreground color when drawing.
     fg: Vec<Option<Color>>,
+    /// Background in each cell; `None` means use default background color when drawing.
     bg: Vec<Option<Color>>,
 }
 
 impl CharGrid {
+    /// Create a new CharGrid with a given [width, height].  Sets white and black as the default
+    /// foreground and background colors respectively.
     pub fn new(size: Size) -> CharGrid {
         let [width, height] = size;
 
@@ -47,6 +67,7 @@ impl CharGrid {
         }
     }
 
+    /// Clear the entire CharGrid.
     pub fn clear(&mut self) {
         for e in self.chars.iter_mut() {
             *e = ' ';
@@ -61,10 +82,13 @@ impl CharGrid {
         }
     }
 
+    /// Put a single character in a given position.
     pub fn put(&mut self, pos: Position, c: char) {
         self.put_color(pos, CellColorArg::Pass, CellColorArg::Pass, c);
     }
 
+    /// Put a single character in a given position, optionally changing the foreground and/or
+    /// background colors.
     pub fn put_color(&mut self, [x, y]: Position, fg: CellColorArg, bg: CellColorArg, c: char) {
         if x >= self.size[0] || y >= self.size[1] {
             return;
@@ -87,10 +111,15 @@ impl CharGrid {
         }
     }
 
+    /// Print a string on the CharGrid starting at the given position.  If the string goes past the
+    /// right edge of the CharGrid it will be truncated.
     pub fn print(&mut self, pos: Position, s: &str) {
         self.print_color(pos, CellColorArg::Pass, CellColorArg::Pass, s);
     }
 
+    /// Print a string on the CharGrid starting at the given position, optionally changing the
+    /// foreground and/or background colors.  If the string goes past the right edge of the
+    /// CharGrid it will be truncated.
     pub fn print_color(&mut self, [x, y]: Position, fg: CellColorArg, bg: CellColorArg, s: &str) {
         let width = self.size[0];
 
@@ -99,6 +128,7 @@ impl CharGrid {
             .for_each(|(i, c)| self.put_color([x + i as u32, y], fg, bg, c));
     }
 
+    /// Draw the whole CharGrid on screen with the given font and font size.
     pub fn draw<G, C>(&self, font_size: u32, cache: &mut C, c: &Context, g: &mut G)
     where
         G: Graphics,
