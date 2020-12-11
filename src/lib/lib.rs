ebb81fa76bd75e979674e4c55a7f16735383cea3
Tung Nguyen <tungtn3@gmail.com>
2020-12-11 19:38:16 +1100

    rename src/lib/a_star.rs to src/lib/path_find.rs

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,14 +1,14 @@
 #[macro_use]
 extern crate bitflags;
 
-mod a_star;
 mod chargrid;
 mod field_of_view;
 mod input_buffer;
+mod path_find;
 mod run;
 
-pub use a_star::{AStarIter, PathableMap};
 pub use chargrid::CharGrid;
 pub use field_of_view::{field_of_view, FovIter, FovShape, ViewableField};
 pub use input_buffer::{InputBuffer, InputEvent, KeyMods};
+pub use path_find::{AStarIter, PathableMap};
 pub use run::{run, RunSettings};
