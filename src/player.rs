ce2d82db6ddf63ec7dcd79fb6539411666a3ec67
Tung Nguyen <tungtn3@gmail.com>
2020-12-12 00:18:36 +1100

    allow player to pass a turn by pressing period

--- src/player.rs
+++ src/player.rs
@@ -51,6 +51,7 @@ pub fn player_input(world: &World, inputs: &mut InputBuffer) -> bool {
             Key::U | Key::NumPad9 => try_move_player(world, 1, -1),
             Key::B | Key::NumPad1 => try_move_player(world, -1, 1),
             Key::N | Key::NumPad3 => try_move_player(world, 1, 1),
+            Key::Period => true,
             _ => false,
         }
     } else {
